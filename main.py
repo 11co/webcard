d3297b413554a6be3f9558691547db12688469c0
11co <aetnea0@gmail.com>
2025-04-27 01:32:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from io import StringIO
 from telebot import TeleBot
 from datetime import datetime, timedelta
 import threading
+import random
 
 BOT_TOKEN = "7411770517:AAGW65ZViNLVCFKFDUM5X-QM15rlxckIb2M"
 WEBHOOK_LIVE = "https://discord.com/api/webhooks/1362353337013506129/bcAAKfveNmZfEQm6KSEXe0_ToWeU_A_hG_jp8kfq7Ga5uBKWQJ8CmBsxFJpmQmMEAItS"
